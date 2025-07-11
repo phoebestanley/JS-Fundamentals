bf915943db8f97629d8c07317eba5d1f05c07652
phoebestanley <phoebewangustanley@gmail.com>
2025-07-11 01:37:35 -0600

    Create 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,11 @@
+const args = process.argv.slice(2);
+const num = Number.parseInt(args[0]);
+if (isNaN(x) || !Number.isInteger(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  let i = 0;
+  while (i < x) {
+    console.log("C is fun");
+    i++;
+  } 
+}
