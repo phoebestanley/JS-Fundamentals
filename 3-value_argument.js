16c0c2ed2767c00604fc169437328614b224f9b7
phoebestanley <phoebewangustanley@gmail.com>
2025-07-11 00:57:33 -0600

    Create 3-value_argument.js

---
+++ 3-value_argument.js
@@ -0,0 +1,6 @@
+const numberOfArguments = Process.argv.length;
+if(numberOfArguments[2] === undefined){
+    console.log('No argument');
+    }
+    else {console.log(numberOfArguments[2]);
+    }   
